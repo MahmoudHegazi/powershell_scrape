32e039369bc07e0f2ed9d57a56b28b76bebaf73e
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-01 09:31:17 +0200

    Create api_copy_paster.ps1

---
+++ api_copy_paster.ps1
@@ -0,0 +1,21 @@
+$WSH = New-Object -Com WScript.Shell
+start chrome 'https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&download=true';
+start-sleep -Seconds 2;
+$WSH.AppActivate("chrome");
+start-sleep -Seconds 2;
+$WSH.sendKeys("^a");
+start-sleep -Seconds 2;
+$WSH.sendKeys("^c");
+start-sleep -Seconds 2;
+start notepad; start-sleep -Seconds 1;
+$WSH.AppActivate("notepad");
+start-sleep -Seconds 2;
+$WSH.sendKeys("^v");
+start-sleep -Seconds 2;
+$filename = [System.IO.Path]::GetRandomFileName().split(".")[0] + ".txt";
+start-sleep -Seconds 1;
+$WSH.sendKeys("^s");
+start-sleep -Seconds 1;
+$WSH.sendKeys($filename);
+start-sleep -Seconds 1;
+$WSH.sendKeys("~");
