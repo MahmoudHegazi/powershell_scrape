278108ecd18c94b6213c9e23e4a872b05145f660
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-01 08:07:06 +0200

    Update power_anticross_scraper.ps1

--- power_anticross_scraper.ps1
+++ power_anticross_scraper.ps1
@@ -8,5 +8,5 @@ $WSH.SendKeys("^+j");
 # Get-Random -Minimum 1 -Maximum 5
 Start-sleep -s 3
 # 31 tab in my case
-For ($i=0; $i -lt 31; $i++) { echo $WSH.SendKeys({TAB}); start-sleep -Seconds 1;  }
+For ($i=0; $i -lt 31; $i++) { $WSH.SendKeys({TAB}); start-sleep -Seconds 1;  }
 $WSH.SendKeys("alert{(}hello world{)};~");
