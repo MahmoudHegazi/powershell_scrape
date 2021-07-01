283a994d7b225ae1e38d9dd24137a1271c890a17
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-01 09:54:15 +0200

    Update power_anticross_scraper.ps1

--- power_anticross_scraper.ps1
+++ power_anticross_scraper.ps1
@@ -1,3 +1,4 @@
+<#
 # $url = 'https://www.google.com';
 # [System.Diagnostics.Process]::Start("chrome.exe","--incognito $url")
 $WSH = New-Object -Com WScript.Shell
@@ -10,3 +11,13 @@ Start-sleep -s 3
 # 31 tab in my case
 For ($i=0; $i -lt 31; $i++) { $WSH.SendKeys({TAB}); start-sleep -Seconds 1;  }
 $WSH.SendKeys("alert{(}hello world{)};~");
+#>
+
+start chrome https://www.google.com; start-sleep -Seconds 2;
+$WSH.AppActivate("chrome");
+start-sleep -Seconds 2;
+$WSH.SendKeys("^+j");
+start-sleep -Seconds 2;
+# this number depend on how many tabs needed to reach the console static for now
+For ($i=0; $i -lt 53; $i++) { start-sleep -Seconds 1; $WSH.SendKeys("{TAB}"); };
+
