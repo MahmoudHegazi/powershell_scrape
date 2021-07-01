647269ccb0046c6e5a881df0e97f40cd65d2ae3f
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-01 07:58:30 +0200

    Update power_anticross_scraper.ps1

--- power_anticross_scraper.ps1
+++ power_anticross_scraper.ps1
@@ -1,9 +1,12 @@
+# $url = 'https://www.google.com';
+# [System.Diagnostics.Process]::Start("chrome.exe","--incognito $url")
 $WSH = New-Object -Com WScript.Shell
-start chrome /incognito https://www.google.com;
+Start("chrome.exe")
+start chrome https://www.google.com;
 $WSH.AppActivate("chrome");
 $WSH.SendKeys("^+j");
 # Get-Random -Minimum 1 -Maximum 5
 Start-sleep -s 3
 # 31 tab in my case
-For ($i=0; $i -lt 31; $i++) { echo $WSH.SendKeys({TAB}); start-sleep -Seconds 10;  }
+For ($i=0; $i -lt 31; $i++) { echo $WSH.SendKeys({TAB}); start-sleep -Seconds 1;  }
 $WSH.SendKeys("alert{(}hello world{)};~");
