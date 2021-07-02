02cc4ee64359c246559edba7f4c7f4814c5e7132
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-02 12:46:47 +0200

    Update mouse_control.ps1

--- mouse_control.ps1
+++ mouse_control.ps1
@@ -6,3 +6,5 @@ $screen.width;
 $screen.height;
 # move cursor into center
 [Windows.Forms.Cursor]::Position = "$($screen.Width - $screen.Width / 2),$($screen.Height - $screen.Height / 2)";
+# clear console
+$WSH.AppActivate('chrome'); Start-sleep -Seconds 1; $WSH.SendKeys("^+j"); Start-sleep -s 2; $WSH.SendKeys("^l");
