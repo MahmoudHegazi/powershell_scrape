75415bfb86dee9cb7bb5af49c5a94327fe03a078
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-07-02 12:27:56 +0200

    Update mouse_control.ps1

--- mouse_control.ps1
+++ mouse_control.ps1
@@ -4,3 +4,5 @@ $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen;
 $screen.width;
 # screen height
 $screen.height;
+# move cursor into center
+[Windows.Forms.Cursor]::Position = "$($screen.Width - $screen.Width / 2),$($screen.Height - $screen.Height / 2)";
